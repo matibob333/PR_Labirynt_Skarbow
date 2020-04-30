76afafa4fe6a847a1aadf007385f1ced6db630f9
Quetzonarch <52034258+Quetzonarch@users.noreply.github.com>
2020-04-30 22:28:29 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -1,46 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define NUMBER_OF_CLIENTS 4
-#define PORT 420
-#define SIZE_OF_DATA 1024
-#define STRING_LENGTH 20
 
 #include<windows.h>
 #include<stdio.h>
 #include<time.h>
+#include "../common/common_structures.h"
 
 #pragma comment(lib, "Ws2_32.lib")
-
-//struktura danych gracza
-typedef struct Player_type
-{
-	//sk³adowa pozioma pozycji gracza
-	int x;
-	//sk³adowa pionowa pozycji gracza
-	int y;
-	//punkty gracza
-	int points;
-	//nick gracza
-	char nick[STRING_LENGTH];
-}Player_type;
-
-//struktura mapy
-typedef struct Map_type
-{
-	//liczba graczy
-	int number_of_players;
-	//tablica graczy
-	Player_type* players;
-}Map_type;
 
 //struktura danych przesy³anych do klienta
 typedef struct Thread_args
 {
-	//gniazdo danego gracza do obs³ugi
-	SOCKET socket;
 	//numer danego gracza
 	int player_number;
 	//wskaŸnik na wspóln¹ mapê
 	Map_type* map;
+	//wskaznik na zmienna, czy gracze sa gotowi
+	int* everybody_ready;
+
 }Thread_args;
 
 //serializacja danych z mapy do tablicy bajtów w celu przes³ania
@@ -48,14 +24,43 @@ void serialize_map(char* data, Map_type* map)
 {
 	char int_holder[sizeof(int)];
 	strcpy(data, "");
-	for (int i = 0; i < map->number_of_players; i++)
+	for (int i = 0; i < NUMBER_OF_CLIENTS; i++)
 	{
-		memcpy(int_holder, &(map->players[i].x), sizeof(int));
-		strcat(data, int_holder);
-		memcpy(int_holder, &(map->players[i].y), sizeof(int));
-		strcat(data, int_holder);
-		memcpy(int_holder, &(map->players[i].points), sizeof(int));
-		strcat(data, int_holder);
+		if (map->players[i].connected)
+		{
+			memcpy(int_holder, &(map->players[i].x), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].y), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].points), sizeof(int));
+			strcat(data, int_holder);
+		}
+	}
+}
+
+void serialize_map_fully(char* data, Map_type* map)
+{
+	char int_holder[sizeof(int)];
+	strcpy(data, "");
+
+	for (int i = 0; i < NUMBER_OF_CLIENTS; i++)
+	{
+		if (map->players[i].connected)
+		{
+			memcpy(int_holder, &(map->players[i].x), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].y), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].ready), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].connected), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(map->players[i].points), sizeof(int));
+			strcat(data, int_holder);
+			memcpy(int_holder, &(strlen(map->players[i].nick), sizeof(int));
+			strcat(data, int_holder);
+			strcat(data, map->players[i].nick);
+		}
 	}
 }
 
@@ -64,35 +69,95 @@ DWORD WINAPI client_thread(void* args)
 	char buf[STRING_LENGTH];
 	int lost_client = 0;
 	Thread_args* arguments = (Thread_args*)args;
+	int player_number = arguments->player_number;
+	SOCKET client_socket = arguments->map->players[player_number].socket;
 	int byte_no = SIZE_OF_DATA;
+	srand(time(NULL) * GetCurrentThreadId());
+	while (!(*(arguments->everybody_ready)))
+	{
+		if (recv(client_socket, buf, STRING_LENGTH, 0) > 0)
+		{
+			//gracz do³¹cza do gry
+			if (strcmp(buf, "connect"))
+			{
+				recv(client_socket, buf, STRING_LENGTH, 0);
+				arguments->map->players[player_number].points = 0;
+				int x = rand() % argumets->map->size;
+				int y = rand() % argumets->map->size;
+				arguments->map->players[player_number].x = x;
+				arguments->map->players[player_number].y = y;
+				arguments->map->players[player_number].connected = 1;
+				arguments->map->players[player_number].ready = 1;
+				strcpy(arguments->map->players[player_number].nick, buf);
+			}
+			//gracz zg³asza gotowoœæ
+			else if (strcmp(buf, "ready"))
+			{
+				arguments->map->players[player_number].ready = 1;
+			}
+			//gracz zg³asza brak gotowoœci
+			else if (strcmp(buf, "not_ready"))
+			{
+				arguments->map->players[player_number].ready = 0;
+			}
+			//gracz od³¹cza siê od rozrywki
+			else if (strcmp(buf, "disconnect"))
+			{
+				arguments->map->players[number_of_player].connected = 0;
+				//zakoñczenie w¹tku
+				free(arguments);
+				return 0;
+			}
+			else if (strcmp(buf, "ping"))
+			{
+				
+			}
+			else
+			{
+				//TODO
+			}
+		}
+		int rd = 1;
+		for (int i = 0; i < NUMBER_OF_CLIENTS; i++)
+		{
+			if (arguments->map->players[i].connected)
+			{
+				if (!arguments->map->players[i].ready)
+				{
+					rd = 0;
+				}
+			}
+		}
+		*(arguments->everybody_ready) = rd;
+	}
 	while (!lost_client)
 	{
-		if (recv(arguments->socket, buf, STRING_LENGTH, 0) > 0)
+		if (recv(client_socket, buf, STRING_LENGTH, 0) > 0)
 		{
 			if (strcmp(buf, "up") == 0)
 			{
-				printf("Player number %d sent GURA\n", arguments->player_number);
-				arguments->map->players[arguments->player_number].y--;
+				printf("Player number %d sent GURA\n", player_number);
+				arguments->map->players[player_number].y--;
 			}
 			else if (strcmp(buf, "down") == 0)
 			{
-				printf("Player number %d sent DUU\n", arguments->player_number);
-				arguments->map->players[arguments->player_number].y++;
+				printf("Player number %d sent DUU\n", player_number);
+				arguments->map->players[player_number].y++;
 			}
 			else if (strcmp(buf, "right") == 0)
 			{
-				printf("Player number %d sent PRAWO\n", arguments->player_number);
-				arguments->map->players[arguments->player_number].x++;
+				printf("Player number %d sent PRAWO\n", player_number);
+				arguments->map->players[player_number].x++;
 			}
 			else if (strcmp(buf, "left") == 0)
 			{
-				printf("Player number %d sent LEWO\n", arguments->player_number);
-				arguments->map->players[arguments->player_number].x--;
+				printf("Player number %d sent LEWO\n", player_number);
+				arguments->map->players[player_number].x--;
 			}
 			else if (strcmp(buf, "ping") == 0)
 			{
 				strcpy(buf, "PONG");
-				send(arguments->socket, buf, STRING_LENGTH, 0);
+				send(client_socket, buf, STRING_LENGTH, 0);
 			}
 			else
 			{
@@ -108,7 +173,7 @@ DWORD WINAPI client_thread(void* args)
 		//alokowana pamiêæ do danych do przesy³ania - zak³adamy nieprzekroczenie 1kB danych - mo¿liwe do zmiany!
 		char* data = (char*)malloc(byte_no);
 		serialize_map(data, arguments->map);
-		send(arguments->socket, data, byte_no, 0);
+		send(client_socket, data, byte_no, 0);
 		free(data);
 	}
 	free(arguments);
@@ -117,9 +182,14 @@ DWORD WINAPI client_thread(void* args)
 
 int main() {
 	Map_type map;
-	map.number_of_players = 0;
+	int everybody_ready = 0;
 	Thread_args *thread_args;
-	Player_type players[NUMBER_OF_CLIENTS];	map.players = players;
+	Player_type players[NUMBER_OF_CLIENTS];
+	for (int i = 0; i < NUMBER_OF_CLIENTS; i++) 
+	{
+		players.connected = 0;
+	}
+	map.players = players;
 	WSADATA wsas;
 	int result;
 	WORD wersja;
@@ -154,55 +224,20 @@ int main() {
 		printf("Client accepted\n");
 		
 		thread_args = (Thread_args*)malloc(sizeof(Thread_args));
-		thread_args->socket = si;
-		players[map.number_of_players].x=0;
-		players[map.number_of_players].y=0;
-		players[map.number_of_players].points=0;
-		strcpy(players[map.number_of_players].nick,"BOB");
-		map.number_of_players++;
-		thread_args->player_number = map.number_of_players;
+		thread_args->everybody_ready = &everybody_ready;
+		for(int i=0;i<NUMBER_OF_CLIENTS;i++)
+		{
+			if(!players[i].connected)
+			{
+				players[i].socket=si;
+				thread_args->player_number = i;
+				break;
+			}
+		}
+		
+		
 		thread_args->map = &map;
 		CreateThread(NULL, 0, client_thread, (void*)thread_args,0, &id);
-		//trzeba dac do nowego watku i odpowienio konczyc petle
-		/*while (1)
-		{
-			if (recv(si, buf, 30, 0) > 0) {
-				buf[5] = '\0';
-				printf("Received %s from client, sending PONG\n", buf);
-			}
-			else {
-				printf("ERROR\n");
-			}
-			strcpy(buf, "PONG");
-			send(si, buf, 30, 0);
-		}*/
-		if (recv(si, buf, STRING_LENGTH, 0) > 0)
-		{
-			if (strcmp(buf, "up") == 0)
-			{
-				printf("GURA\n");
-			}
-			else if (strcmp(buf, "down") == 0)
-			{
-				printf("DUU\n");
-			}
-			else if (strcmp(buf, "right") == 0)
-			{
-				printf("PRAWO\n");
-			}
-			else if (strcmp(buf, "left") == 0)
-			{
-				printf("LEWO\n");
-			}
-			else
-			{
-				printf("NIEZROZUMIALE\n");
-			}
-		}
-		else
-		{
-			printf("ERROR\n");
-		}
 	}
 	closesocket(si);
 	WSACleanup();
